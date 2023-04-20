56787f099fd8c6d18c423dd04f6e7cdee7101e81
gavividra <gvidra@ucsd.edu>
2023-04-20 11:02:22 -0700

    new code

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void newTestReverseInPlace(){
+    int[] input1 = {1,2,3,4,5,6};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{6,5,4,3,2,1}, input1);
+  }
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void newTestReversed(){
+    int[] input1 = {1,2,3,4};
+    assertArrayEquals(new int[]{4,3,2,1}, ArrayExamples.reversed(input1));
   }
 }
